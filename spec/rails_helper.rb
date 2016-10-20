4f6f9fbcb118e9766170920b7cba6c35d11581b8
Chase Dunagan <cdunagan@utexas.edu>
2016-10-20 11:47:12 -0600

    add vcr into gem file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,9 +4,11 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-
-
 require 'capybara/rails'
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
