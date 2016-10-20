160eff37d9224238545198d68324d07671cae7de
Calaway <Christopher.Calaway@gmail.com>
2016-10-19 21:38:32 -0600

    Move files regarding 2 factor from lib to models.

--- lib/confirmation_sender.rb
+++
@@ -1,7 +0,0 @@
-module ConfirmationSender
-  def self.send_confirmation_to(user)
-    verification_code = CoderGenerator.generate
-    user.update(verification_code: verification_code)
-    MessageSender.send_code(user.user_profile.phone_number, verification_code)
-  end
-end
