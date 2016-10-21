a62ad92da7baad262cdcd444fb60884b08e0370c
Daniel Broadbent <dbroadbent85@gmail.com>
2016-10-20 18:20:38 -0600

    fix fabricator error

--- spec/features/logged_in_users/user_creates_album_comment_spec.rb
+++ spec/features/logged_in_users/user_creates_album_comment_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.feature "User adds comment to album" do
   scenario "logged in user comments on album" do
     user = Fabricate(:user)
-    album = Fabricate(:album)
+    album = Fabricate(:album, user_id: user.id)
     stub_login_user(user)
-    visit '/albums/1'
+    visit "/albums/#{album.id}"
     fill_in 'Comment', with: 'hello world'
     click_on 'Add Comment'
-    expect(current_path).to eq('/albums/1')
+    expect(current_path).to eq("/albums/#{album.id}")
     expect(page).to have_content('hello world')
     expect(page).to have_content(user.username)
     expect(page).to have_content(Comment.last.created_at)
