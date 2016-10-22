259d14b6b48ea37ed309be75038c195558faea09
Calaway <Christopher.Calaway@gmail.com>
2016-10-22 14:58:54 -0600

    Create permissions service object.

---
+++ app/services/permission.rb
@@ -0,0 +1,25 @@
+class Permission
+  def initialize(user:, controller:, action:)
+    @user = user || User.new
+    @controller = controller
+    @action = action
+  end
+
+  def authorized?
+    if user.admin?
+      return true if controller == "home" && action.in?(%w(index))
+      false
+    elsif user.registered?
+      return true if controller == "home" && action.in?(%w(index))
+      false
+    else
+      return true if controller == "home" && action.in?(%w(index))
+      false
+    end
+  end
+
+  private
+    attr_reader :user,
+                :controller,
+                :action
+end
