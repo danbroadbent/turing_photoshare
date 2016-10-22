932c7da377d8e0115d886d978b0ee18e5f335eec
Calaway <Christopher.Calaway@gmail.com>
2016-10-22 14:59:26 -0600

    Starting to work on route permissions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :authorize!
+
   helper_method :current_user
 
   def current_user
     @user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def guest_user?
-    session[:user_id].nil?
+  # def guest_user?
+  #   session[:user_id].nil?
+  # end
+
+  def current_permission
+    Permission.new(user: current_user,
+                   controller: params[:controller],
+                   action: params[:action])
+  end
+
+  def authorize!
+    unless current_permission.authorized?
+      redirect_to root_url, danger: "The page you requested is not available or does not exist."
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
-  has_secure_password
-  validates :username, presence: true, uniqueness: true
   has_many :albums
   has_many :photos
+
+  has_secure_password
+
+  enum role: %w(registered admin)
+  before_validation :set_role
+
+  validates :username, presence: true, uniqueness: true
+
+  def set_role
+    self.role ||= 0
+  end
+
+  def admin?
+    self.role == "admin"
+  end
+
+  def registered?
+    self.role == "registered"
+  end
 end
