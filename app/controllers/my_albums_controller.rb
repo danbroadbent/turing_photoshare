3c676cf966262e5ae15ec2c603038fdfc923c07e
Calaway <Christopher.Calaway@gmail.com>
2016-10-22 18:47:52 -0600

    Build test for Logged in visits my albums path.

---
+++ app/controllers/my_albums_controller.rb
@@ -0,0 +1,5 @@
+class MyAlbumsController < ApplicationController
+  def index
+    @albums = Album.where(user_id: current_user.id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/my_albums', to: 'my_albums#index'
 end
