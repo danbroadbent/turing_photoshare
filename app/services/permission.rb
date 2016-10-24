7b6c114af2198298d5cb7784f3c94bb1220035d5
Calaway <Christopher.Calaway@gmail.com>
2016-10-24 16:55:21 -0600

    Delete home controller as it became obsolete.

--- app/services/permission.rb
+++ app/services/permission.rb
@@ -7,7 +7,6 @@ class Permission
 
   def authorized?
     if user.admin?
-      return true if controller == "home" && action.in?(%w(index))
       return true if controller == "albums" && action.in?(%w(index show new create destroy))
       return true if controller == "users" && action.in?(%w(show))
       return true if controller == "confirmations" && action.in?(%w(new create))
@@ -19,12 +18,10 @@ class Permission
       return true if controller == "album_users" && action.in?(%w(new create))
       false
     elsif user.registered?
-      return true if controller == "home" && action.in?(%w(index))
       return true if controller == "albums" && action.in?(%w(index show new create destroy))
       return true if controller == "users" && action.in?(%w(show update))
       return true if controller == "confirmations" && action.in?(%w(new create))
       return true if controller == "comments" && action.in?(%w(create destroy edit update))
-
       return true if controller == "photos" && action.in?(%w(new create))
       return true if controller == "my_albums" && action.in?(%w(index))
       return true if controller == "sessions" && action.in?(%w(destroy))
@@ -32,7 +29,6 @@ class Permission
       return true if controller == "album_users" && action.in?(%w(new create))
       false
     else
-      return true if controller == "home" && action.in?(%w(index))
       return true if controller == "albums" && action.in?(%w(index))
       return true if controller == "users" && action.in?(%w(new create))
       return true if controller == "sessions" && action.in?(%w(new create))
