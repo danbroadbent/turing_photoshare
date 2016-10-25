08d2c0cc25da853177399b0e1dd55e37c0cee0e9
Chase Dunagan <cdunagan@utexas.edu>
2016-10-24 18:31:18 -0600

    add sad path to delete comments api test

--- spec/requests/api/v1/delete_comment_request_spec.rb
+++ spec/requests/api/v1/delete_comment_request_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe "Comments edit" do
     expect(response['body']).to eq(nil)
     expect(Comment.count).to eq(0)
   end
+
+  it "edits a comment on an album with wrong token" do
+    user = Fabricate(:user, api_token: '12345')
+    album = Fabricate(:album)
+    Fabricate(:album_user, user: user, album: album)
+    comment = Fabricate(:comment, album: album)
+
+    delete "/api/v1/albums/#{album.id}/comments/#{comment.id}.json?api_token=#{SecureRandom.hex}"
+
+    expect(response.status).to eq(403)
+  end
 end
