b47cc5d2b2d81d5837b9af16b148c88b380763b6
PlanetEfficacy <jspevack@gmail.com>
2016-10-26 09:14:30 -0600

    add feature test for private shared albums of deactivated users are not accessible

---
+++ spec/features/deactivated_user/private_shared_albums_no_longer_show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "User with private shared album is deactivated" do
+  scenario "that album is no longer viewable by other album users" do
+    deactive_user = Fabricate(:user, active: false)
+    active_user = Fabricate(:user)
+    album = Fabricate(:album)
+    Fabricate(:album_user,
+              user: deactive_user,
+              album: album,
+              owner: true)
+    Fabricate(:album_user,
+              user: active_user,
+              album: album)
+
+    stub_login_user(active_user)
+    visit albums_path
+    expect(page).to_not have_content(album.title)
+
+    visit album_path(album)
+    expect(current_page).to eq(albums_path)
+    expect(page).to have_content("The album you are looking for belongs to an inactive user and is no longer accessible.")
+  end
+end
